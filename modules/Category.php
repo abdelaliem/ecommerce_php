66b96456ea76ab36eb9c3233ce7d4b9661441f48
marbella267 <135547136+marbella267@users.noreply.github.com>
2024-01-05 20:03:40 +0200

    get the category data

--- modules/Category.php
+++ modules/Category.php
@@ -1,7 +1,17 @@
 <?php
-class category extends product
+require 'product.php';
+class Category extends product 
 {
     public $category_name;
-
+    public function GetCategory($id=0){
+         if($id!=0){
+            $query = "SELECT * FROM `category` WHERE `category_id`=$id";
+            $data = $this -> con -> conn -> query($query) -> fetch_all(MYSQLI_ASSOC);
+            return $data ; 
+         }
+        $query = "SELECT * FROM `category` ";
+        $data = $this -> con -> conn -> query($query) -> fetch_all(MYSQLI_ASSOC);
+        return $data ; 
+    }
     
 }
